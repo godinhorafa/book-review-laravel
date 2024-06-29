9ff608e382e19ecbeb02fba87bd5642a6c8102b1
rafaella godinho <rafaellagodinho@outlook.com>
2024-06-29 20:44:19 -0300

    method for most popular and highest rated books

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -13,9 +13,18 @@ class Book extends Model
     {
         return $this->hasMany(Review::class);
     }
-
     public function scopeTitle(Builder $query, string $title): Builder
     {
         return $query->where('title','LIKE','%'. $title .'%');
+    }
+
+    public function scopePopular(Builder $query): Builder
+    {
+        return $query->withCount('reviews')->orderBy('reviews_count', 'desc');
+    }
+
+    public function scopeHighestRated(Builder $query): Builder
+    {
+        return $query->withAvg('reviews', 'rating')->orderBy('reviews_avg_rating', 'desc');
     }
 }
