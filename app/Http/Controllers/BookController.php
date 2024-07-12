f896c94633f0e1e28e48db44a1203f25759dd4b2
rafaella godinho <rafaellagodinho@outlook.com>
2024-07-12 12:26:26 -0300

    caching books

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Book;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class BookController extends Controller
 {
@@ -26,7 +27,9 @@ class BookController extends Controller
         default => $books->latest()
        };
 
-       $books = $books->get();
+    //    $books = $books->get();
+
+      $books =  Cache::remember(('books', 3600, fn() => $books->get());
 
         return view('books.index', ['books' => $books]);
     }
--- app/Models/User.php
+++ app/Models/User.php
@@ -11,6 +11,10 @@ class User extends Authenticatable
 {
     use HasFactory, Notifiable;
 
+    /**
+     *
+     * @var array<int, string>
+     */
     protected $fillable = [
         'name',
         'email',
